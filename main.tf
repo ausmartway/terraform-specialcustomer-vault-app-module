b7f2da5045607d579099a48c6a59594d0456ea1a
Yulei Liu <yulei.liu@gmail.com>
2021-09-08 11:40:00 +1000

    add support for approle per enviroment

--- main.tf
+++ main.tf
@@ -43,10 +43,11 @@ path "${var.appname}/*" {
 EOT
 }
 
+//Approle is intended when there is no better/native authentication method, eg, aws/gcp/azure/k8s/ldap.
 //create approle role that uses secret-provider policy
 resource "vault_approle_auth_backend_role" "secret-provider-approle" {
   count = length(var.enviroments)
-  backend        = vault_auth_backend.approle.path
+  backend        = var.approle_path
   role_name      = "${var.appname}-${var.enviroments[count.index]}-secret-provider"
   token_policies = ["${var.appname}-${var.enviroments[count.index]}-secret-provider"]
 }
@@ -54,7 +55,7 @@ resource "vault_approle_auth_backend_role" "secret-provider-approle" {
 //create approle role that uses secret-consumer policy
 resource "vault_approle_auth_backend_role" "secret-consumer-approle" {
   count = length(var.enviroments)
-  backend        = vault_auth_backend.approle.path
+  backend        = var.approle_path
   role_name      = "${var.appname}-${var.enviroments[count.index]}-secret-consumer"
   token_policies = ["${var.appname}-${var.enviroments[count.index]}-secret-consumer"]
 }
@@ -62,7 +63,7 @@ resource "vault_approle_auth_backend_role" "secret-consumer-approle" {
 //create approle role that uses secret-admin policy
 resource "vault_approle_auth_backend_role" "secret-admin-approle" {
   count = length(var.enviroments)
-  backend        = vault_auth_backend.approle.path
+  backend        = var.approle_path
   role_name      = "${var.appname}-${var.enviroments[count.index]}-secret-admin"
   token_policies = ["${var.appname}-${var.enviroments[count.index]}-secret-admin"]
 }
--- variables.tf
+++ variables.tf
@@ -6,3 +6,9 @@ variable "enviroments" {
   description = "list of enviroments"
   default     = ["prod", "dev"]
 }
+
+variable approle_path {
+  type        = string
+  default     = "approle"
+  description = "The path of AppRole auth backend, eg, approle"
+}
