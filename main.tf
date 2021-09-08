124903802f4affa0abb8ef530cba1d800eda5631
Yulei Liu <yulei.liu@gmail.com>
2021-09-08 11:17:28 +1000

    enable approle roles for application

--- main.tf
+++ main.tf
@@ -34,11 +34,35 @@ EOT
 //create a policy that allows both read/write permission for the application per enviroment. 
 resource "vault_policy" "secretadmin" {
   count = length(var.enviroments)
-  name  = "${var.appname}-${var.enviroments[count.index]}-secretadmin"
+  name  = "${var.appname}-${var.enviroments[count.index]}-secret-admin"
 
   policy = <<EOT
 path "${var.appname}/*" {
   capabilities = ["read","create", "update", "delete", "list"]
 }
 EOT
+}
+
+//create approle role that uses secret-provider policy
+resource "vault_approle_auth_backend_role" "secret-provider-approle" {
+  count = length(var.enviroments)
+  backend        = vault_auth_backend.approle.path
+  role_name      = "${var.appname}-${var.enviroments[count.index]}-secret-provider"
+  token_policies = ["${var.appname}-${var.enviroments[count.index]}-secret-provider"]
+}
+
+//create approle role that uses secret-consumer policy
+resource "vault_approle_auth_backend_role" "secret-consumer-approle" {
+  count = length(var.enviroments)
+  backend        = vault_auth_backend.approle.path
+  role_name      = "${var.appname}-${var.enviroments[count.index]}-secret-consumer"
+  token_policies = ["${var.appname}-${var.enviroments[count.index]}-secret-consumer"]
+}
+
+//create approle role that uses secret-admin policy
+resource "vault_approle_auth_backend_role" "secret-admin-approle" {
+  count = length(var.enviroments)
+  backend        = vault_auth_backend.approle.path
+  role_name      = "${var.appname}-${var.enviroments[count.index]}-secret-admin"
+  token_policies = ["${var.appname}-${var.enviroments[count.index]}-secret-admin"]
 }
